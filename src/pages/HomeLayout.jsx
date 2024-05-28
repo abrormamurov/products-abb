d2400a62278372191cde1ba0492a1dd3a2d19f07
abrormamurov <mabrorbk@gmail.com>
2024-05-28 16:17:56 +0500

    ad

---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Header, Navbar } from "../components";
+function HomeLayout() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default HomeLayout;
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Hero } from "../components";
+
+function Landing() {
+  return (
+    <div className="align-element mt-10">
+      <Hero />
+    </div>
+  );
+}
+
+export default Landing;
